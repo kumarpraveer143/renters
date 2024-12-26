fb99070e8037dcba39a7851ada2e79c69f9a9229
kumarpraveer143 <kumarpraveernit@gmail.com>
2024-12-26 22:59:54 +0530

    converted the tailwind config file in es6

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// @type {import('tailwindcss').Config}
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include React component files
   ],
