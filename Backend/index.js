003a8322ca4950b173be4e8bc55612acb6bfc9d2
kumarpraveer143 <kumarpraveernit@gmail.com>
2025-03-29 18:09:14 +0530

    local cors configured

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,8 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  // origin: "https://renters-8vii985d4-renters-projects.vercel.app",
-  origin: "https://renters-eight.vercel.app",
+  origin: "http://localhost:5173/",
+  // origin: "https://renters-eight.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
