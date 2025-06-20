c42a8360c30fae87ec6601b6124e16f69b44f2d9
kumarpraveer143 <kumarpraveernit@gmail.com>
2025-06-21 05:12:05 +0530

    changed cors

--- Backend/index.js
+++ Backend/index.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5173",
-  // origin: "https://renters-eight.vercel.app",
+  // origin: "http://localhost:5173",
+  origin: "https://renterservice.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
